de8bdedec47d8d83b6be2efaf04591eb487e3393
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-05-10 14:02:00 -0600

    Make sure no user passwords expire

--- setup-autologin.ps1
+++ setup-autologin.ps1
@@ -1,3 +1,4 @@
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /f /v DefaultUserName /d ft
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /f /v DefaultPassword /d $args[0]
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /f /v AutoAdminLogon /d "1"
+wmic UserAccount set PasswordExpires=False
